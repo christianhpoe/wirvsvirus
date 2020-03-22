2053e555b8cfd95af522ae45ece1f562149d2ea0
Malte Klingenberg <malteklingenberg1@gmail.com>
2020-03-22 13:21:30 +0100

    submit createPage funktioniert

--- app.py
+++ app.py
@@ -220,6 +220,17 @@ def logout():
 @login_required
 def createPage():
     form = CreatePageForm()
+    
+    if form.validate_on_submit():
+      page = Page()
+      form.populate_obj(page)
+      page.creator_id = current_user.id
+      
+      print("adding ", page)
+      db.session.add(page)
+      db.session.commit()
+      return redirect(url_for('index'))
+    
     return render_template("createPage.html", title="Seite erstellen", form=form)
 
 @app.route("/deletePage", methods=['GET', 'POST'])
